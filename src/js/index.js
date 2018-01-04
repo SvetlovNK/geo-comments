fd067754e6e07fb30bc037801070853d2c7a7168
Nickolay Svetlov <ns@idaproject.com>
2018-01-04 13:32:25 +0300

    add popup to balloon

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,15 @@
+const templateElement = require('../../popup.hbs');
+
 export default function () {
     let map = {
         objects: {},
         yMap: undefined,
+        cluster:undefined,
         init: function() {
             (async() => {
                 try {
                     let map = this.yMap = await this.mapInit();
+                    this.addClusterization();
 
                     map.events.add('click', this.bindClick.bind(this));
 
@@ -43,23 +47,37 @@ export default function () {
                     });
             });
         },
+        addClusterization: function () {
+            this.clusterer = new ymaps.Clusterer({
+                preset: 'islands#invertedVioletClusterIcons',
+                groupByCoordinates: false,
+                clusterDisableClickZoom: true,
+                clusterHideIconOnBalloonOpen: false,
+            });
+        },
         bindClick: function (event) {
             let coords = event.get('coords');
-            console.log(this);
+            const html = templateElement();
+
             this.yMap.balloon.open(coords, {
-                // contentHeader:'Событие!',
-                contentBody:'<p>Кто-то щелкнул по карте.</p>' +
-                '<p>Координаты щелчка: ' + [
-                    coords[0].toPrecision(6),
-                    coords[1].toPrecision(6)
-                ].join(', ') + '</p>',
-                // contentFooter:'<sup>Щелкните еще раз</sup>'
+                contentBody: html,
+            },{
+                closeButton: false,
+                maxHeight: 'auto',
+                layout: 'default#imageWithContent',
+                style: {
+                    background: 'black'
+                },
             });
+
+            console.log(this.yMap.balloon.isOpen());
 
             this.createMarker(coords);
         },
         createMarker: function (coordinates) {
             this.yMap.geoObjects.add(new ymaps.Placemark(coordinates));
+
+            console.log('add');
         }
     };
 
